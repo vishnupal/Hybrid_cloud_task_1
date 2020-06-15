9acd1d5e6d6c377d6a61223dbdb8c5ac8a255c67
vishnupal <36783980+vishnupal@users.noreply.github.com>
2020-06-16 02:45:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,7 @@
 <?php
 
       
-$myfile = fopen("cloudfrount.txt", "r") or die("Unable to open file!");
 
-echo " <img src='$a/images/img1.jpg' /> ";
 echo "<img src='http://$a/images/img1.jpg' alt='Girl in a jacket' style='width:50%;height:60%'/>";
 echo "";
 fclose($myfile);
